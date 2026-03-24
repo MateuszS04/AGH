1a45376030aba3f3b81f32b8487e2deaaeab6993
MateuszS04 <mateuszsondej04@gmail.com>
2026-03-24 12:32:38 +0100

    fix submodule

--- sem_4/www/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll(".fade-in").forEach(function(element) {
-      element.classList.add("show");
-    });
-  });
